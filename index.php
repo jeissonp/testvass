dbdee46794323d171b76bf4ea9f5de373e6a0ade
jeissonp <jeissonp@gmail.com>
2019-06-12 22:50:33 -0500

    cors

--- index.php
+++ index.php
@@ -7,6 +7,23 @@
  * All Drupal code is released under the GNU General Public License.
  * See COPYRIGHT.txt and LICENSE.txt files in the "core" directory.
  */
+$http_origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : NULL;
+
+if ($http_origin) {
+  header("Access-Control-Allow-Origin: $http_origin");
+} else {
+  header("Access-Control-Allow-Origin: *");
+}
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Headers: Cookie');
+header('X-XSS-Protection: 1; mode=block');
+if($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+  header('Access-Control-Allow-Headers: X-CSRF-Token, Authorization, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+  //header("HTTP/1.1 200 OK");
+  die();
+} else {
+  header('Access-Control-Allow-Headers: Content-Type, Authorization, Accept');
+}
 
 use Drupal\Core\DrupalKernel;
 use Symfony\Component\HttpFoundation\Request;
